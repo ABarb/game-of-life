3c33b9267f0a74106fd58377e5ac7d23e22821bb
Anthony Barbee <abarbee@abarbee-LT.myevolver.local>
2013-07-03 11:02:24 -0400

    Commented. Hopefully the change will be added.

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-	LIVE_CELL("*"), DEAD_CELL(".");
+	LIVE_CELL("*"), DEAD_CELL("."); //Changed to working condition
 	private String symbol;
 
 	private Cell(String symbol) {
